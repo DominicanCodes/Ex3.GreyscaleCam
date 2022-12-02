1146ee7f84ae831747bdf759f013c3b0138889d7
cardandres <65802352+cardandres@users.noreply.github.com>
2022-12-01 21:53:12 -0500

    Add and Implement "Upload a photo" feature Add "Waiting for a photo..." message

--- main.py
+++ main.py
@@ -1,17 +1,32 @@
 import streamlit as st
 from PIL import Image
 
+# Allow uploading photo
+uploaded_image = st.file_uploader("Upload Image")
+
+# print(1, uploaded_image)
+
 with st.expander("Start Camera"):
     # Start the camera
     camera_image = st.camera_input("Camera")
 
-
+# Create a pillow image instance
 if camera_image:
-    # Create a pillow image instance
     img = Image.open(camera_image)
+elif uploaded_image:
+    img = Image.open(uploaded_image)
+else:
+    img = None
 
+message = st.empty()
+# print(2, img, type(img) == 'PIL.PngImagePlugin.PngImageFile')
+
+if (img==None):
+    message.info("Waiting for a photo...")    
+else:
     # Convert the pillow image to grayscale
     gray_img = img.convert("L")
 
     # Render the grayscale image on a webpage
     st.image(gray_img)
+    # print(True)
